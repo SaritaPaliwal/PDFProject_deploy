927825d3dffbffcec9ee346674664876f77d7f41
sarita <saritapaliwal566@gmail.com>
2025-12-17 11:56:20 +0530

    allowed

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
